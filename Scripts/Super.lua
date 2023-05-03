967798750a6b9f333d93567a50fab341e85ce439
miyuyumi <miyu.alts@gmail.com>
2023-05-03 23:03:04 +0800

    Changed Config

--- Scripts/Super.lua
+++ Scripts/Super.lua
@@ -158,7 +158,7 @@ else
 end
 
 task.spawn(function()
-    task.wait(40)
+    task.wait(60)
     getgenv().AuthKey = getgenv().HugeKey
     getgenv().LoadSettings = {
         UseSeperateSaves = true,
