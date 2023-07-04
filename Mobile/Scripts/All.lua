20dac5b79726b3484c95efe399226c9814cf9e2f
miyuyumi <miyu.alts@gmail.com>
2023-07-04 11:43:52 +0800

    Changed Config

--- Mobile/Scripts/All.lua
+++ Mobile/Scripts/All.lua
@@ -38,6 +38,55 @@ function Webhook(Url, Data)
 end
 
 if game.PlaceId ~= 7722306047 then
+    task.spawn(function()
+        repeat
+            task.wait()
+        until game.CoreGui:FindFirstChild('RobloxPromptGui')
+
+        local promptOverlay = game.CoreGui.RobloxPromptGui.promptOverlay
+
+        sendError = function()
+            errorMessage = promptOverlay.ErrorPrompt.MessageArea.ErrorFrame.ErrorMessage.Text
+            url = getgenv().DebugHook
+
+            if string.match(errorMessage, "unexpected client behavior") or string.match(errorMessage, "Please rejoin%.") then
+                Webhook(url, {
+                    ["content"] = "@everyone",
+                    ["embeds"] = {{
+                        ["title"] = game:GetService("Players").LocalPlayer.Name .. " has been disconnected!",
+                        ["description"] = errorMessage,
+                        ["type"] = "rich",
+                        ["color"] = tonumber(0x7269da)
+                    }}
+                })
+            else
+                Webhook(url, {
+                    ["embeds"] = {{
+                        ["title"] = game:GetService("Players").LocalPlayer.Name .. " has been disconnected!",
+                        ["description"] = errorMessage,
+                        ["type"] = "rich",
+                        ["color"] = tonumber(0x7269da)
+                    }}
+                })
+            end
+        end
+
+        promptOverlay.ChildAdded:connect(function(V)
+            if V.Name == 'ErrorPrompt' then
+                repeat
+                    if promptOverlay.ErrorPrompt.MessageArea.ErrorFrame.ErrorMessage.Text ~= "Label" then
+                        sendError()
+                        -- local getResponse = makeGetRequest(apiUrl)
+                        -- game:GetService("TeleportService"):TeleportToPlaceInstance(7722306047, getResponse,
+                        --     game.Players.LocalPlayer)
+                        game:Shutdown()
+                    end
+                    task.wait(5)
+                until false
+            end
+        end)
+    end)
+
     local count = 0
     while game.PlaceId ~= 7722306047 do
         count = count + 1
